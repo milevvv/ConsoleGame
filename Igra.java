437dacd902be93bec8386d005f447804c986e1c0
SIMEON MILEV <monimilev7@gmail.com>
2023-06-01 18:39:08 +0300

    asd

--- Igra.java
+++ Igra.java
@@ -2,76 +2,83 @@ import java.util.Random;
 
 public class Igra {
 	public static void main(String[] args) throws InterruptedException {
+		// в началото
 		Random r = new Random();
-			
-		for(int n=0; n < 25; n++) {
-			int p1r = r.nextInt(1, 10);
-			int rez1 = 0;
-			int rez2 = 0;
-				 
-				 
-				 
-				 
-				 
-			System.out.println(p1r+ "                                          ");	 
-			System.out.println("                     " +rez1+":"+rez2 + "            ");
-			System.out.println("--------                             --------");
-			System.out.println("|                                           |");
-			System.out.println("|       *                            *      |");
-			System.out.println("|      /|\\                          /|\\     |");
-			System.out.println("|       |                            |      |");
-			// най-долен ред
-			System.out.print("|      / \\");
-			for(int i =0; i<25; i++) {
-				if (n == i) {
-					System.out.print("o");
-				}else if(n != i){
-					System.out.print(" ");
-				}
-			}
-			System.out.println(" / \\     |");
-			// до тук
-			
-			System.out.println("|___________________________________________|");
-			Thread.sleep(50);
-			
-			
-			rez1++;
-		}
-		for(int n=25; n > 0; n--) {
-			int p2r = r.nextInt(1, 10);
-			
-				 
-			int rez1 = 0;
-			int rez2 = 0;
-				 
-				 
-				 
-				 
-			System.out.println("                                          " + p2r);	 
-			System.out.println("                     " +rez1+":"+rez2 + "            ");
-			System.out.println("--------                             --------");
-			System.out.println("|                                           |");
-			System.out.println("|       *                            *      |");
-			System.out.println("|      /|\\                          /|\\     |");
-			System.out.println("|       |                            |      |");
-			// най-долен ред
-			System.out.print("|      / \\");
-			for(int i =0; i<25; i++) {
-				if (n == i) {
-					System.out.print("o");
-				}else if(n != i){
-					System.out.print(" ");
-				}
-			}
-			System.out.println(" / \\     |");
-			// до тук
-			
-			System.out.println("|___________________________________________|");
-			Thread.sleep(50);
+		int rez1 = 0;
+		int rez2 = 0;
 		
-			rez2++;	
+		while(true) {
+			// едно разиграване
+			int p1r = r.nextInt(1, 10);
+			int p2r = r.nextInt(1, 10);
+			if(p1r>7) {
+				rez1++;	
+			 }
+			// топката тръгва отляво надясно
+		for(int n=0; n < 25; n++) {
+			// топката се премества с едно надясно
+			
+			
+			
+			
+			
+				 
+				 
+				 
+			
+			 
+			System.out.println(p1r+"                   " +rez1+":"+rez2 + "            "+p2r);
+			System.out.println("--------                             --------");
+			System.out.println("|                                           |");
+			System.out.println("|       *                            *      |");
+			System.out.println("|      /|\\                          /|\\     |");
+			System.out.println("|       |                            |      |");
+			// най-долен ред
+			System.out.print("|      / \\");
+			for(int i =0; i<25; i++) {
+				if (n == i) {
+					System.out.print("o");
+				}else if(n != i){
+					System.out.print(" ");
+				}
+			} 
+			System.out.println(" / \\     |");
+			// до тук
+			
+			System.out.println("|___________________________________________|");
+			Thread.sleep(100);
+			
+			
 		}
+		if(p2r>7) {
+			rez2++;	
+		 }
+		for(int n=25; n > 0; n--) {
+		
+			
+			
+			System.out.println(p1r+ "                  " +rez1+":"+rez2 + "            "+p2r);
+			System.out.println("--------                             --------");
+			System.out.println("|                                           |");
+			System.out.println("|       *                            *      |");
+			System.out.println("|      /|\\                          /|\\     |");
+			System.out.println("|       |                            |      |");
+			// най-долен ред
+			System.out.print("|      / \\");
+			for(int i =0; i<25; i++) {
+				if (n == i) {
+					System.out.print("o");
+				}else if(n != i){
+					System.out.print(" ");
+				}
+			}
+			System.out.println(" / \\     |");
+			// до тук
+			
+			System.out.println("|___________________________________________|");
+			Thread.sleep(100);
+			
+		
 		 
 		//Igrachi g1 = new Igrachi("Cristiano Ronaldo", "Nike Zoom Mercurial Dream Speed Superfly 9 Elite FG", 7, 1.87, 37);
 		//Igrachi g2 = new Igrachi("Lionel Messi", "Adidas X SPEEDPORTAL MESSI.1 FG", 10, 1.70, 34); 
@@ -105,7 +112,7 @@ public class Igra {
 				
 				
 		
-		         
-		         
+		}
+		}     
 	}
 }

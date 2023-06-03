7ec1256b9c15007d0ffa2f283bbbfc79e2440a84
milevvv <101856935+milevvv@users.noreply.github.com>
2023-06-03 23:55:45 +0300

    Update Igra.java

--- Igra.java
+++ Igra.java
@@ -2,21 +2,45 @@ import java.util.Random;
 
 public class Igra {
 	public static void main(String[] args) throws InterruptedException {
-		// в началото
+		
+		Igrachi g1 = new Igrachi("Cristiano Ronaldo", "Nike Zoom Mercurial Dream Speed Superfly 9 Elite FG", 7, 1.87, 37);
+		Igrachi g2 = new Igrachi("Lionel Messi", "Adidas X SPEEDPORTAL MESSI.1 FG", 10, 1.70, 34); 
+		Igrachi g3 = new Igrachi("Neymar Jr", "Puma Future Ultimate", 11, 1.75, 31);
+		Igrachi g4 = new Igrachi("Sadio Mane", "New Balance Furon v6+", 17, 1.74, 30);
+		
+		 
+		 
+		String g1Ime = g1.getIme();
+		System.out.println(g1Ime);
+		
+		int g1Godini = g1.getGodini();
+		System.out.println(g1Godini); 
+		 
+		int g1Nomer = g1.getNomer();
+		System.out.println(g1Nomer);
+		 
+		double g1Visochina = g1.getVisochina();
+		System.out.println(g1Visochina);
+		 
+		String g1Butonki = g1.getButonki();
+		System.out.println(g1Butonki);
+		
+		
+		
 		Random r = new Random();
 		int rez1 = 0;
 		int rez2 = 0;
 		
 		while(true) {
-			// едно разиграване
+			
 			int p1r = r.nextInt(1, 10);
 			int p2r = r.nextInt(1, 10);
 			if(p1r>7) {
 				rez1++;	
 			 }
-			// топката тръгва отляво надясно
+			
 		for(int n=0; n < 25; n++) {
-			// топката се премества с едно надясно
+			
 			
 			
 			
@@ -33,7 +57,7 @@ public class Igra {
 			System.out.println("|       *                            *      |");
 			System.out.println("|      /|\\                          /|\\     |");
 			System.out.println("|       |                            |      |");
-			// най-долен ред
+			
 			System.out.print("|      / \\");
 			for(int i =0; i<25; i++) {
 				if (n == i) {
@@ -43,7 +67,7 @@ public class Igra {
 				}
 			} 
 			System.out.println(" / \\     |");
-			// до тук
+			
 			
 			System.out.println("|___________________________________________|");
 			Thread.sleep(100);
@@ -63,7 +87,7 @@ public class Igra {
 			System.out.println("|       *                            *      |");
 			System.out.println("|      /|\\                          /|\\     |");
 			System.out.println("|       |                            |      |");
-			// най-долен ред
+			
 			System.out.print("|      / \\");
 			for(int i =0; i<25; i++) {
 				if (n == i) {
@@ -73,34 +97,14 @@ public class Igra {
 				}
 			}
 			System.out.println(" / \\     |");
-			// до тук
+			
 			
 			System.out.println("|___________________________________________|");
 			Thread.sleep(100);
 			
 		
 		 
-		//Igrachi g1 = new Igrachi("Cristiano Ronaldo", "Nike Zoom Mercurial Dream Speed Superfly 9 Elite FG", 7, 1.87, 37);
-		//Igrachi g2 = new Igrachi("Lionel Messi", "Adidas X SPEEDPORTAL MESSI.1 FG", 10, 1.70, 34); 
-		//Igrachi g3 = new Igrachi("Neymar Jr", "Puma Future Ultimate", 11, 1.75, 31);
-		//Igrachi g4 = new Igrachi("Sadio Mane", "New Balance Furon v6+", 17, 1.74, 30);
 		
-		 
-		 
-		// String g1Ime = g1.getIme();
-		// System.out.println(g1Ime);
-		
-		// int g1Godini = g1.getGodini();
-		// System.out.println(g1Godini); 
-		 
-		// int g1Nomer = g1.getNomer();
-		// System.out.println(g1Nomer);
-		 
-		// double g1Visochina = g1.getVisochina();
-		// System.out.println(g1Visochina);
-		 
-		// String g1Butonki = g1.getButonki();
-		// System.out.println(g1Butonki);
 		     
 		   
 			
